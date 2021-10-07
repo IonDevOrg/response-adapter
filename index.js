5e62772818014a9a5f29f320e55114087bb5bcef
Daniel Farias <daniel.dsfarias@gmail.com>
2021-10-07 19:26:53 -0300

    Add .editorconfig

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const FORM_URLENCODED = 'application/x-www-form-urlencoded';
-const { parse } = require('querystring');
+const FORM_URLENCODED = "application/x-www-form-urlencoded";
+const { parse } = require("querystring");
 
 JSON.safeStringify = (obj, indent = 4) => {
-    let cache = []
+    let cache = [];
     let retVal = JSON.stringify(
         obj,
         (key, value) =>
@@ -13,113 +13,115 @@ JSON.safeStringify = (obj, indent = 4) => {
                 : value,
         indent
     );
-    cache = null
-    return retVal
-}
+    cache = null;
+    return retVal;
+};
 
 function collectAwsData({ isBase64, body }) {
     try {
-        if(isBase64){
-            body = Buffer.from(body, 'base64').toString('utf8')
+        if (isBase64) {
+            body = Buffer.from(body, "base64").toString("utf8");
         }
-        body = JSON.parse(body)
+        body = JSON.parse(body);
     } catch (error) {
-        console.log(error)
-        body = {}
+        console.log(error);
+        body = {};
     } finally {
-        return body
+        return body;
     }
 }
 
 function collectRequestData(request) {
     return new Promise((resolve, reject) => {
         let data = "";
-            
-        request.on('data', function(chunk){ 
+
+        request.on("data", function (chunk) {
             data += chunk.toString();
-        })
+        });
 
         request.on("error", (error) => {
-            reject(error)
-        })
+            reject(error);
+        });
 
-        if(request.headers['content-type'] === FORM_URLENCODED) {
-            request.on('end', () => {
+        if (request.headers["content-type"] === FORM_URLENCODED) {
+            request.on("end", () => {
                 resolve(parse(data));
-            })
-            
-        }else if(request.headers['content-type'] && request.headers['content-type'].toLowerCase().includes('json')){
-            request.on('end', function(){
-                resolve(JSON.parse(data))
-            })
+            });
+        } else if (
+            request.headers["content-type"] &&
+            request.headers["content-type"].toLowerCase().includes("json")
+        ) {
+            request.on("end", function () {
+                resolve(JSON.parse(data));
+            });
+        } else {
+            resolve({});
         }
-        else {
-            resolve({})
-        }
-    })
-    
+    });
 }
 
 class WebResponseAdapter {
-
     constructor(response = false) {
-        this.body = {}
-        this.statusCode = 200
-        this.response = response
+        this.body = {};
+        this.statusCode = 200;
+        this.response = response;
         // Check if AWS:
-        this.isAWSRequest = !response
+        this.isAWSRequest = !response;
         // Check if Normal Node:
-        if(response){
-            this.isPlainRequest = true
+        if (response) {
+            this.isPlainRequest = true;
         }
     }
 
-    status(statusCode){
-        this.statusCode = statusCode
-        return this
+    status(statusCode) {
+        this.statusCode = statusCode;
+        return this;
     }
 
-    json(body){
-        this.body = body
-        return this
+    json(body) {
+        this.body = body;
+        return this;
     }
 
-    end(){
-        if(this.isPlainRequest){
-            this.response.writeHead(this.statusCode, {'Content-Type': 'application/json'})
-            return this.response.end(JSON.safeStringify(this.body)) 
+    end() {
+        if (this.isPlainRequest) {
+            this.response.writeHead(this.statusCode, {
+                "Content-Type": "application/json",
+            });
+            return this.response.end(JSON.safeStringify(this.body));
         }
         //
         return {
             statusCode: this.statusCode,
-            body: JSON.safeStringify(this.body)
-        }    
+            body: JSON.safeStringify(this.body),
+        };
     }
 }
 
 class WebRequestAdapter {
-
     constructor(request) {
-        this.request = request 
+        this.request = request;
         // Check if AWS:
-        this.isAWSRequest = request.resource && request.path && request.httpMethod
+        this.isAWSRequest =
+            request.resource && request.path && request.httpMethod;
         // Check if Normal Node:
-        this.isPlainRequest = request.url && request.method && request._readableState
-        this.method = this.isAWSRequest ? request.httpMethod : request.method
+        this.isPlainRequest =
+            request.url && request.method && request._readableState;
+        this.method = this.isAWSRequest ? request.httpMethod : request.method;
     }
 
-    async getBody(){
+    async getBody() {
         //
-        if(this.isAWSRequest){
-            this.body = collectAwsData(this.request)
-        }else{
-            this.body = await collectRequestData(this.request)
+        if (this.isAWSRequest) {
+            this.body = collectAwsData(this.request);
+        } else {
+            this.body = await collectRequestData(this.request);
         }
-        return this.body
+        return this.body;
     }
 }
 
 module.exports = {
     WebResponseAdapter,
-    WebRequestAdapter
+    WebRequestAdapter,
 };
